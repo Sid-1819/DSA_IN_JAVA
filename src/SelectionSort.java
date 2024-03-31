eead1b72cafc0c7f4c6b95fee2b6d812dee709e7
Siddhesh Shirdhankar <110876355+Sid-1819@users.noreply.github.com>
2024-03-31 10:58:56 +0530

    Selection Sort

---
+++ src/SelectionSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int []arr= {4,3,7,8,1};
+       Selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static int getMaxIndex(int[]arr,int start,int end){
+        int max =start;
+        for (int i = start; i <=end; i++) {
+            if (arr[max]<arr[i]){
+                max=i;
+            }
+
+
+        }return max;
+    }
+    static void swap(int []arr, int first, int second){
+int temp = arr[first];
+arr[first]= arr[second];
+arr[second]=temp;
+    }
+    static void Selection(int []arr){
+        for(int i=0 ;i<=arr.length-1;i++) {
+           int  end = arr.length - i - 1;
+           int maxIndex= getMaxIndex(arr,0,end);
+            swap(arr,maxIndex,end);
+        }
+
+
+
+    }
+}
